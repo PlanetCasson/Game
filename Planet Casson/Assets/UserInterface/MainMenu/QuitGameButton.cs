8e569742042bde0ee50b1dd020c6ca533501a6cf
AwdeDarkar <darkardengeno@gmail.com>
2018-04-10 22:14:49 -0400

    Cleaned up the UI and made a Main Menu

---
+++ Planet Casson/Assets/UserInterface/MainMenu/QuitGameButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Model;
+using UnityEngine.SceneManagement;
+
+public class QuitGameButton : MonoBehaviour
+{
+
+    public Button quit_btn;
+
+    void Start()
+    {
+        Button btn = quit_btn.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        #if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
---
+++ Planet Casson/Assets/UserInterface/MainMenu/StartGameButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Model;
+using UnityEngine.SceneManagement;
+
+public class StartGameButton : MonoBehaviour
+{
+
+    public Button start_btn;
+
+    void Start()
+    {
+        Button btn = start_btn.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("TestScene", LoadSceneMode.Single);
+    }
+}
