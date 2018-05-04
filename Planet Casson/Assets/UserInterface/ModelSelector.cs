48fada7df0f8613e6239aace37b6ccd5ee114e17
AwdeDarkar <darkardengeno@gmail.com>
2018-05-04 13:57:57 -0400

    Improved the model selector

--- Planet Casson/Assets/UserInterface/ModelSelector.cs
+++ Planet Casson/Assets/UserInterface/ModelSelector.cs
@@ -7,11 +7,14 @@ using System.IO;
 public class ModelSelector : MonoBehaviour
 {
     public Dropdown dropdown_select;
+    public GameObject master;
     public List<string> models;
+    private SphereKernel kernel;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
+        kernel = master.GetComponent<SphereKernel>();
         models = new List<string>();
         DirectoryInfo d = new DirectoryInfo(Application.dataPath + "/StreamingAssets/");//Assuming Test is your Folder
         FileInfo[] Files = d.GetFiles("*.obj"); //Getting Text files;
@@ -21,11 +24,20 @@ public class ModelSelector : MonoBehaviour
             Debug.Log(file.Name);
         }
         dropdown_select.AddOptions(models);
+        dropdown_select.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(dropdown_select);
+        });
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    void DropdownValueChanged(Dropdown change)
     {
-		
-	}
+        foreach (Transform child in master.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        string url = models[change.value];
+        kernel.changeModel(url);
+    }
+
+    void Update() { }
 }
