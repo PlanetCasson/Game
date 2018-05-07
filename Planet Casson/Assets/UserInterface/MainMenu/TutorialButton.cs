654e89fcfbf5f30a097c540580d7ee7e2e9f751f
Parker Ballner <pballner@gmail.com>
2018-05-07 14:14:11 -0400

    Final Bug fixes and updates. Added tutorial

---
+++ Planet Casson/Assets/UserInterface/MainMenu/TutorialButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Model;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Starts the game with the first model in the StreamingAssets folder
+/// </summary>
+public class TutorialButton : MonoBehaviour
+{
+
+	public Button tutorial_btn;
+
+	void Start()
+	{
+		Button btn = tutorial_btn.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		SceneManager.LoadScene("Tutorial", LoadSceneMode.Single);
+	}
+}
